1081df13640e7e734b910931887e9d497562319b
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2022-04-25 19:01:12 +0530

    card section all done

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,12 @@
 
 import React from 'react';
-import HeaderContainer from '../container/header';
+// import HeaderContainer from '../container/header';
 import CardContainer from '../container/card';
 
 export default function Home() {
   return (
     <>
-      <HeaderContainer />
+      {/* <HeaderContainer /> */}
       <CardContainer />
     </>
   )
