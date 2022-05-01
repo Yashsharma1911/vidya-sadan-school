7ae566d7926a949cf4926d966801a4871871bbc5
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2022-05-01 16:08:28 +0530

    Jumbotron created with responsiveness without any bug

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,12 +2,14 @@
 import React from 'react';
 // import HeaderContainer from '../container/header';
 import CardContainer from '../container/card';
+import JumbotronContainer from '../container/jumbotron';
 
 export default function Home() {
   return (
     <>
       {/* <HeaderContainer /> */}
       <CardContainer />
+      <JumbotronContainer />
     </>
   )
 }
