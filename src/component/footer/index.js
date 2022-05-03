101521a99e814e40473bac98ec970a454a2e531d
yash sharma <yashsharma2572@gmail.com>
2022-05-03 16:56:41 +0530

    created footer successfully

--- src/component/footer/index.js
+++ src/component/footer/index.js
@@ -1,6 +1,80 @@
-import React from 'react'
+import React from "react";
+import {
+  Container,
+  Text,
+  Row,
+  Column,
+  PageLink,
+  ImgSocialIcon,
+  ImgIcon,
+  Logo,
+  Div,
+  CopyRightDiv,
+  RowWrap,
+} from "./styles/footer";
+import { Link } from "react-router-dom";
 
 export default function Footer({ children, ...restProps }) {
-    return <Container {...restProps}>{children}</Container>
+  return <Container {...restProps}>{children}</Container>;
 }
 
+Footer.Text = function FooterText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Footer.Row = function FooterRow({ children, ...restProps }) {
+  return <Row {...restProps}>{children}</Row>;
+};
+
+Footer.Column = function FooterColumn({ children, ...restProps }) {
+  return <Column {...restProps}>{children}</Column>;
+};
+
+Footer.PageLink = function FooterPageLink({ mailto, children, ...restProps }) {
+  return (
+    <PageLink
+      {...restProps}
+      onClick={(e) => {
+        window.location.href = mailto;
+        e.preventDefault();
+      }}
+    >
+      {children}
+    </PageLink>
+  );
+};
+
+Footer.ImgSocialIcon = function FooterImgSocialIcon({ to, ...restProps }) {
+  return (
+    <Link to={to}>
+      <ImgSocialIcon {...restProps} />
+    </Link>
+  );
+};
+
+Footer.ImgIcon = function FooterImgIcon({ ...restProps }) {
+  return <ImgIcon {...restProps} />;
+};
+
+Footer.Logo = function FooterLogo({ to, ...restProps }) {
+  return (
+    <Link to={to}>
+      <Logo {...restProps} />
+    </Link>
+  );
+};
+
+Footer.Div = function FooterDiv({ children, ...restProps }) {
+  return <Div {...restProps}>{children}</Div>;
+};
+
+Footer.RowWrap = function FooterRowWrap({ children, ...restProps }) {
+  return <RowWrap {...restProps}>{children}</RowWrap>;
+};
+
+Footer.CopyRightDiv = function FooterDivCopyRightDiv({
+  children,
+  ...restProps
+}) {
+  return <CopyRightDiv {...restProps}>{children}</CopyRightDiv>;
+};
